5900c570bf242fbe004a7b56c32a2f99cf6b87b5
visualstudio <visualstudio@122-PC26>
2019-04-10 10:58:43 -0400

    Section B

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -11,7 +11,8 @@ namespace ConsoleApp5
     class Program
     {
         ArrayList Beowulf;
-        int LineCount = 0;
+        int counterletters = 0;
+        int countSpaces = 0;
         static void Main(string[] args)
         {
             Program p = new Program();
@@ -22,45 +23,50 @@ namespace ConsoleApp5
         public void ReadTextFiles()
         {
             // Read file using StreamReader.reads file line by line
-            using (StreamReader file = new StreamReader("U:/Users/730283/Downloads/Beowulf.txt"))
+            using (StreamReader sr  = new StreamReader("U:/Users/730283/Downloads/Beowulf.txt"))
             {
+                string line;
                 int counter = 0;
-                string ln;
-                while ((ln = file.ReadLine()) != null)
+                int a = 0, myWord = 1;
+
+                while ((line = sr.ReadLine()) != null)
                 {
-                    Console.WriteLine(ln);
-                    Beowulf.Add(ln);
+                    Console.WriteLine(line);
+                    Beowulf.Add(line);
+                    FindNumberOfBlankSpaces(line);
+                    counter++;
+
+                    while (a <= line.Length - 1)
+                    {
+                        if (line[a] == ' ' || line[a] == '\n' || line[a] == '\t')
+                        {
+                            myWord++;
+                        }
+                        a++;
+                    }
+                    a = 0;
 
                 }
-                file.Close();
-                Console.WriteLine($"File has {counter} lines.");
+
+
+                Console.WriteLine("\n\n\n\n********************************The number of lines in the paragraph is " + counter);
+                Console.WriteLine("The number of words in paragraph is " + myWord);
+                Console.ReadLine();
             }
+
         }
         public int FindNumberOfBlankSpaces(string line)
         {
-            int countletters = 0;
-            int countSpaces = 0;
+
+
             foreach (char c in line)
             {
-                if (char.IsLetter(c)) { countletters++; }
+                if (char.IsLetter(c)) { counterletters++; }
                 if (char.IsWhiteSpace(c)) { countSpaces++; }
             }
             return countSpaces;
-
-        }
-        public void Line()
-        {
-            string line;
-            TextReader reader = new StreamReader("U: /Users/730263/downloads/Beowulf.txt");
-            while ((line = reader.ReadLine()) != null)
-            {
-                LineCount++;
-            }
-            reader.Close();
-            Console.WriteLine(LineCount);
         }
     }
 }
 
-     
-   
+
