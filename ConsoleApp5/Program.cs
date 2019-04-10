e723a536b43ea6a06fce7e8abd637fc06a858ebf
visualstudio <visualstudio@122-PC26>
2019-04-10 10:20:54 -0400

    Section A

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp5
     class Program
     {
         ArrayList Beowulf;
+        int LineCount = 0;
         static void Main(string[] args)
         {
             Program p = new Program();
@@ -25,16 +26,41 @@ namespace ConsoleApp5
             {
                 int counter = 0;
                 string ln;
-                while ((ln = file.ReadLine()) !=null)
+                while ((ln = file.ReadLine()) != null)
                 {
                     Console.WriteLine(ln);
                     Beowulf.Add(ln);
-                        
+
                 }
                 file.Close();
                 Console.WriteLine($"File has {counter} lines.");
             }
         }
-     
+        public int FindNumberOfBlankSpaces(string line)
+        {
+            int countletters = 0;
+            int countSpaces = 0;
+            foreach (char c in line)
+            {
+                if (char.IsLetter(c)) { countletters++; }
+                if (char.IsWhiteSpace(c)) { countSpaces++; }
+            }
+            return countSpaces;
+
+        }
+        public void Line()
+        {
+            string line;
+            TextReader reader = new StreamReader("U: /Users/730263/downloads/Beowulf.txt");
+            while ((line = reader.ReadLine()) != null)
+            {
+                LineCount++;
+            }
+            reader.Close();
+            Console.WriteLine(LineCount);
+        }
     }
 }
+
+     
+   
